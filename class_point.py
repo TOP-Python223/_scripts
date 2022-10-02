e452cfab15bc6baa557e40d07c86a8da9849ec38
Gennadiy <renardes@gmail.com>
2022-10-02 19:36:05 +0500

    перегрузка операторов сравнения <, > и ==

--- class_point.py
+++ class_point.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from numbers import Real
 
 
 class Point:
@@ -13,6 +14,7 @@ class Point:
     def __str__(self):
         return f'({self.x}; {self.y})'
 
+    # point1 - point2
     def __sub__(point1, point2: 'Point') -> float:
         """Вычисляет и возвращает длину отрезка, заданного двумя точками."""
         if type(point2) is point1.__class__:
@@ -22,7 +24,44 @@ class Point:
         else:
             raise TypeError
 
+    # self < other
+    def __lt__(self, other) -> bool:
+        if isinstance(other, self.__class__):
+            return self.x < other.x and self.y < other.y
+        elif isinstance(other, int):
+            return self.x < other and self.y < other
+        else:
+            raise TypeError
+
+    # self > other
+    def __gt__(self, other) -> bool:
+        if isinstance(other, self.__class__):
+            return self.x > other.x and self.y > other.y
+        elif isinstance(other, int):
+            return self.x > other and self.y > other
+        else:
+            raise TypeError
+
+    # self == other
+    def __eq__(self, other) -> bool:
+        if isinstance(other, self.__class__):
+            return self.x == other.x and self.y == other.y
+        elif isinstance(other, int):
+            return self.x == other and self.y == other
+        else:
+            raise TypeError
+
+
 
 p1 = Point(0, 0)
 p2 = Point(1, 1)
-print(p2 - p1)
+print(p2 - p1, end='\n\n')
+
+p3 = Point(2, 3)
+p4 = Point(1, 4)
+print(f'{p1 < p2 = }')
+print(f'{p3 > p4 = }')
+# то же самое, что и: not p1.__eq__(p4)
+print(f'{p1 != p4 = }')
+# не определено
+# print(f'{p2 <= p4 = }')
