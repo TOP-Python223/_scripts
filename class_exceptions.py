48e208a5f52c6463210af8a144e134f6ad4a26b0
Gennadiy <renardes@gmail.com>
2022-10-08 20:44:30 +0500

    проверка работоспособности

--- class_exceptions.py
+++ class_exceptions.py
@@ -19,10 +19,18 @@ class TextProcessor:
         )
 
 
+test_text = """A Current Directory for each drive?
+Originally in MS-DOS, each drive had its own current directory, for complex historical reasons.
+
+Now in Win32, there is one global current directory, but at the command line the appearance is still maintained that each drive has its own current directory, this is a fake-out by cmd.exe.
+The location for each drive is stored using the undocumented environment variables =A:, =B:, =C: etc.
+
+The only reason you need to be aware of this is that GUI Windows applications may have a different current directory than the command prompt. Similarly two CMD sessions can each have a different current directory."""
+
 exc = None
-tp = TextProcessor('')
+tp = TextProcessor(test_text)
 try:
-    tp.clear_non_letters()
+    print(tp.clear_non_letters(), end='\n'*3)
 except EmptyStringError as e:
     exc = e
 
